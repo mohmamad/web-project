525ef0d2fda0e9c7722487b78ba0ed693aa853fb
mohammadIsa <mohamad.isa.moghrabi@gmail.com>
2022-05-07 19:28:09 +0300

    redo the back end

--- travel.php
+++ travel.php
@@ -20,14 +20,231 @@
 
     <title>Outdoors</title>
 
-    <?php
 
-    include "controller.php";
+    <?php
+    function creat_conn(){
+        $host="localhost";
+        $name="root";
+        $pass="";
+        $dbname="web-project";
+        $conn=new mysqli($host,$name,$pass,$dbname);
+
+        if(mysqli_connect_errno()){
+            echo "cant connect to data base";
+        }
+        else{
+            return $conn;
+        }
+    }
+
+    $action = isset($_GET['action']) ? $_GET['action'] : "";
 
     ?>
-=======
-     include "controller.php";
-     ?>
+
+    <?php
+
+   // adding a user functions
+    function add_new_user($user_obj)
+    {
+        $conn = creat_conn();
+        $prepare = $conn->prepare("INSERT INTO `user` (`first-name`,`last-name`, `password`,`phone-number`, `email`) VALUES (?,?,?,?,?)");
+
+        try {
+            $prepare->bind_param('sssss', $user_obj['firstname'], $user_obj["lastname"], $user_obj["password"], $user_obj["phonenumber"], $user_obj["email"]);
+
+            $prepare->execute();
+
+            return "success";
+
+        } catch (Exception $e) {
+
+                return "wrong";
+
+        }
+    }
+
+
+    function create_new_user()
+    {
+        $email = "";
+        $firstname = "";
+        $lastname = "";
+        $password = "";
+        $repassword = "";
+        $phonenumber = "";
+        if (isset($_POST["email"])) {
+            $email = $_POST["email"];
+        }
+        if (isset($_POST["firstname"])) {
+            $firstname = $_POST["firstname"];
+        }
+        if (isset($_POST["lastname"])) {
+            $lastname = $_POST["lastname"];
+        }
+        if (isset($_POST["password"])) {
+            $password = $_POST["password"];
+        }
+        if (isset($_POST["repassword"])) {
+            $repassword = $_POST["repassword"];
+        }
+        if (isset($_POST["phonenumber"])) {
+            $phonenumber = $_POST["phonenumber"];
+        }
+
+        if ($password == $repassword && $password != "") {
+
+            $user_obj["email"] = $email;
+            $user_obj["firstname"] = $firstname;
+            $user_obj["lastname"] = $lastname;
+            $user_obj["password"] = $password;
+            $user_obj["repassword"] = $repassword;
+            $user_obj["phonenumber"] = $phonenumber;
+
+            $result = add_new_user($user_obj);
+
+            if ($result == "success") {
+                ?>
+
+                <div class="alert alert-success">
+                    <strong>Success you can login using this acount!</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+               <?php
+    } else {
+                ?>
+
+                <div class="alert alert-danger">
+                    <strong>the email exists!</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <?php
+            }
+
+        } else {
+            ?>
+
+            <div class="alert alert-danger">
+                <strong>the password and confirmation password does not match!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <?php
+        }
+
+    }
+
+    ?>
+
+<!--    login functions-->
+    <?php
+
+
+    function check_login_email($email)
+    {
+
+        $item = "";
+        $conn = creat_conn();
+        try {
+            $prepare = $conn->prepare("select * from `user` where email=?");
+            $prepare->bind_param('s', $email);
+            $prepare->execute();
+            $result = $prepare->get_result();
+            $item = $result->fetch_object();
+
+            return $item;
+
+        } catch (Exception $e) {
+            return $item;
+        }
+    }
+
+    function check_login_password($password)
+    {
+        $item = "";
+        $conn = creat_conn();
+        try {
+            $prepare = $conn->prepare("select * from `user` where password=?");
+            $prepare->bind_param('s', $password);
+            $prepare->execute();
+            $result = $prepare->get_result();
+            $item = $result->fetch_object();
+
+            return $item;
+        } catch (Exception $e) {
+
+            return $item;
+        }
+    }
+
+
+    function get_login_data()
+    {
+        $email = "";
+        $password = "";
+        if (isset($_POST["login-email"])) {
+            $email = $_POST["login-email"];
+        }
+        if (isset($_POST["login-password"])) {
+            $password = $_POST["login-password"];
+        }
+
+        $emailresult = check_login_email($email);
+
+
+        if ($emailresult == "") {
+            ?>
+
+            <div class="alert alert-danger">
+                <strong>no matched email or password!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <?php
+        } else {
+            $passwordresult = check_login_password($password);
+            if($passwordresult == ""){
+                ?>
+
+                <div class="alert alert-danger">
+                    <strong>no matched email or password!</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <?php
+            }
+            else {
+                ?>
+
+                <div class="alert alert-success">
+                    <strong>login successfully</strong>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <?php
+            }
+        }
+    }
+
+
+    ?>
+
+
+
+
 
 </head>
 <body>
@@ -50,7 +267,7 @@
                 <li>
                     <a href="http://localhost:63342/webProject/Attraction.html?_ijt=cih8aga574s0ri0abhjqnerdt4&_ij_reload=RELOAD_ON_SAVE">Attraction</a>
                 </li>
-=======
+
                 <li class="active"><a href="http://localhost:63342/webProject/travel.html?_ijt=nfl8040gbfj027ian69dghumid&_ij_reload=RELOAD_ON_SAVE#">Home</a></li>
                 <li><a href="http://localhost:63342/webProject/Attraction.html?_ijt=cih8aga574s0ri0abhjqnerdt4&_ij_reload=RELOAD_ON_SAVE">Attraction</a></li>
 
@@ -63,11 +280,7 @@
                 <!--                sign in-->
                 <li><a data-toggle="modal" data-target="#exampleModalCenter" href="#"><span
                                 class="glyphicon glyphicon-log-in"></span> Login</a></li>
-=======
-                        class=" glyphicon glyphicon-user "> </span>Sign Up</a></li>
-                <!--                sign in-->
-                <li><a data-toggle="modal" data-target="#exampleModalCenter" href="#"><span
-                        class="glyphicon glyphicon-log-in"></span> Login</a></li>
+
 
             </ul>
         </div>
@@ -125,8 +338,7 @@
                 </div>
                 <div class="modal-body">
                     <div class="shape">
-
-                        <form method="POST" action="controller.php?action=sign-in">
+                        <form method="POST" action="?action=sign-in">
 <!--                            email input-->
                             <div class="form-outline mb-4 control-container">
                                 <label class="form-label " for="form2Example1">Email address</label>
@@ -167,37 +379,7 @@
                             </button>
                         </form>
 
-                        <div class="form-outline mb-4 control-container">
-                            <label class="form-label " for="form2Example1">Email address</label>
-                            <input type="email" id="form2Example1" class="form-control"/>
-                        </div>
-                        <!-- Password input -->
-                        <div class="form-outline mb-4 control-container">
-                            <label class="form-label" for="form2Example2">Password</label>
-                            <input type="password" id="form2Example2" class="form-control"/>
-                        </div>
 
-                        <!-- 2 column grid layout for inline styling -->
-                        <div class="row mb-4 control-container">
-                            <div class="col d-flex justify-content-center">
-                                <!-- Checkbox -->
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="form2Example31"
-                                           checked/>
-                                    <label class="form-check-label" for="form2Example31"> Remember me </label>
-                                </div>
-                            </div>
-
-                            <div class="col">
-                                <!-- forgetting password -->
-                                <a href="#!">Forgot password?</a>
-                            </div>
-                        </div>
-
-                        <!-- Submit button -->
-                        <button type="button" class="btn btn-block mb-4 control-container main-color">Sign
-                            in
-                        </button>
 
 
                         <!-- Register buttons -->
@@ -229,11 +411,7 @@
                 <div class="modal-body">
                     <div class="signup-form sign-up-design">
                         <p>Please fill in this form to create an account!</p>
-
-                        <form method="POST" action="controller.php?action=newUser">
-
-                        <form  method="POST" action="controller.php?action=newUser">
-
+                        <form method="POST" action="?action=newUser">
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col-xs-6"><input type="text" class="form-control" name="firstname"
@@ -279,50 +457,28 @@
             </div>
         </div>
     </div>
+
+
     <!--      /register  -->
+    <!--    controller -->
+    <?php
+
+    switch ($action) {
+        case "newUser":
+            create_new_user();
+            break;
+        case "sign-in":
+            get_login_data();
+            break;
+
+        default:
+    }
+
+    ?>
 
 </header>
-<div class="error-handling" style="position: absolute;top:10%;width: 100%">
-    <?php
 
-    if (isset($_GET['error'])) {
-        ?>
-        <div class="alert alert-danger">
-            <strong>Danger!</strong> <?php echo $_GET['error']; ?>
-=======
-    if(isset($_GET['error']))
-    {
-        ?>
-        <div class="alert alert-danger">
-            <strong>Danger!</strong> <?php echo $_GET['error'];?>
 
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <?php
-    }
-    ?>
-    <?php
-
-    if (isset($_GET['success'])) {
-        ?>
-        <div class="alert alert-success">
-            <strong>Success!</strong> <?php echo $_GET['success']; ?>
-
-    if(isset($_GET['success']))
-    {
-        ?>
-        <div class="alert alert-success">
-            <strong>Success!</strong> <?php echo $_GET['success'];?>
-
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <?php
-    }
-    ?>
 </div>
 <div style="position: absolute; top: 51%; width: 100%;">
     <h1 style="text-align: center; width: 100%;">Explore the world with us!</h1>
@@ -338,10 +494,6 @@
             <div class="box bg-white" style="margin: 5px;padding-bottom: 0px;" data-work="London">
                 <a style="width:100%; height: 100%; position: absolute " data-toggle="modal"
                    data-target="#conform-flight" href="#"></a>
-
-            <div class="box bg-white"  style="margin: 5px;padding-bottom: 0px;" data-work="London">
-                <a style="width:100%; height: 100%; position: absolute " data-toggle="modal" data-target="#conform-flight" href="#"></a>
-
                 <img src="pics/attraction/londn.jpeg" alt="" class="img-fluid">
             </div>
         </div>
@@ -350,9 +502,6 @@
             <div class="box bg-white" style="margin: 5px;padding-bottom: 0px;" data-work="Maldives">
                 <a style="width:100%; height: 100%; position: absolute " data-toggle="modal"
                    data-target="#conform-flight" href="#"></a>
-
-            <div class="box bg-white"  style="margin: 5px;padding-bottom: 0px;" data-work="Maldives">
-                <a style="width:100%; height: 100%; position: absolute " data-toggle="modal" data-target="#conform-flight" href="#"></a>
 
                 <img src="pics/attraction/maldives.jpg" alt="" class="img-fluid">
             </div>
@@ -363,9 +512,6 @@
                 <a style="width:100%; height: 100%; position: absolute " data-toggle="modal"
                    data-target="#conform-flight" href="#"></a>
 
-            <div class="box bg-white"  style="margin: 5px;padding-bottom: 0px;" data-work="Berlin">
-                <a style="width:100%; height: 100%; position: absolute " data-toggle="modal" data-target="#conform-flight" href="#"></a>
-
                 <img src="pics/attraction/berlin.jpg" alt="" class="img-fluid">
             </div>
         </div>
@@ -374,9 +520,6 @@
             <div class="box bg-white" style="margin: 5px;padding-bottom: 0px;" data-work="Humburg">
                 <a style="width:100%; height: 100%; position: absolute " data-toggle="modal"
                    data-target="#conform-flight" href="#"></a>
-
-            <div class="box bg-white"  style="margin: 5px;padding-bottom: 0px;" data-work="Humburg">
-                <a style="width:100%; height: 100%; position: absolute " data-toggle="modal" data-target="#conform-flight" href="#"></a>
 
                 <img src="pics/attraction/humburg.jpg" alt="" class="img-fluid">
             </div>
@@ -387,8 +530,6 @@
                 <a style="width:100%; height: 100%; position: absolute " data-toggle="modal"
                    data-target="#conform-flight" href="#"></a>
 
-            <div class="box bg-white "  style="margin: 5px;padding-bottom: 0px;" data-work="Istanbul">
-                <a style="width:100%; height: 100%; position: absolute " data-toggle="modal" data-target="#conform-flight" href="#"></a>
 
                 <img src="pics/attraction/istanbul.jpg" alt="" class="img-fluid ">
             </div>
@@ -408,8 +549,6 @@
             <div style="text-align: center; padding-bottom: 2px !important; " class="modal-header">
 
                 <h2 style="display: inline-block; margin-top: 10px !important;">book flight</h2>
-
-                <h2 style="display: inline-block; margin-top: 10px !important;">Add new trip</h2>
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
@@ -471,5 +610,6 @@
 
 </body>
 
-</html>
 
+
+</html>
